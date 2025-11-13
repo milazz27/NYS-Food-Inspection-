21413ffc0406a4d0e11e2b401a9e7cc831115f9c
Mila <milatila@milaspc.localdomain>
2025-11-12 19:58:42 -0500

    main SQL

---
+++ SQL Scripts/main_data.sql
@@ -0,0 +1,35 @@
+ --Table to drop all data into prior to further parsing into other tables
+
+ DROP TABLE IF EXISTS all_rest_data;
+
+ CREATE TABLE all_rest_data(
+    rid VARCHAR(8),
+    facility_name TEXT,
+    address TEXT,
+    last_inspected TEXT,
+    violation_code VARCHAR(3),
+    violation TEXT,
+    violation_count INT,
+    num_crit_not_corrected INT,
+    num_not_critical INT,
+    description TEXT,
+    local_health_dept TEXT,
+    county TEXT,
+    facility_addresss TEXT,
+    city TEXT,
+    zipcode TEXT,
+    nysdoh TEXT,
+    municipality TEXT,
+    operation_name TEXT,
+    permit_exp_date TEXT,
+    permitted TEXT,
+    business TEXT,
+    corp_name TEXT,
+    operator_lname TEXT,
+    operator_fname TEXT,
+    nys_health_id TEXT,
+    inspection_type TEXT,
+    comments TEXT,
+    state TEXT,
+    coords TEXT
+ );
